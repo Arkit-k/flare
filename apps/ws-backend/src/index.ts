2bd4dbc03afdfe005b5d514613993ade71acef66
arkit_k <arkitkarmokar007@gmail.com>
2025-10-23 10:48:58 +0530

    ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,41 @@
 import { JWT_SECRET } from '@repo/backend-common/config'
-import { request } from "http";
+import { IncomingMessage } from "http";
 import { WebSocketServer } from "ws";
+import WebSocket from 'ws';
 import jwt, { JwtPayload } from 'jsonwebtoken'
 
 const wss = new WebSocketServer({ port: 8080});
 
-wss.on('connection' , function connection(ws , request) {
+interface User {
+      ws:WebSocket,
+      rooms: string[],
+      userId: string
+}
+const users:User[] =[] ;
+
+
+function checkUser(token: string | null): JwtPayload | null {
+      if (!token) {
+            return null;
+      }
+
+      try {
+            const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload | string;
+
+            if (typeof decoded == 'string') {
+                  return null;
+            }
+            if (!decoded || !decoded.userId){
+                  return null;
+            }
+
+            return decoded;
+      } catch (e) {
+            return null;
+      }
+}
+
+wss.on('connection' , function connection(ws , request: IncomingMessage) {
       const url = request.url;
       if(!url) {
             return;
@@ -13,15 +43,51 @@ wss.on('connection' , function connection(ws , request) {
 
       const queryParams = new URLSearchParams(url.split('?')[1]);
       const token = queryParams.get('token') || "";
-      const decoded = jwt.verify(token , JWT_SECRET)
+      const userPayload = checkUser(token);
 
-      if (!decoded || !(decoded as JwtPayload).userId) {
-            ws.close();
-            return;
+      if (userPayload === null) {
+            ws.close()
+            return null;
       }
 
-      ws.on('message' , function message(data){
-            ws.send('ping');
+      users.push({
+       userId: userPayload.userId,
+       rooms:[],
+       ws
       })
+
+
+
+      ws.on('message' , function message(data){
+            const parsedata = JSON.parse(data as unknown as string)
+           
+            if (parsedata.type === 'join_room') {
+                  const user = users.find(x => x.ws === ws);
+                  user?.rooms.push(parsedata.roomId)
+            }
+
+            if (parsedata.type === "leave_room") {
+                  const user = users.find(x => x.ws === ws);
+                  if (!user) {
+                        return;
+                  }
+                  user.rooms = user?.rooms.filter(x => x === parsedata.room)
+            }
+
+            if (parsedata.type === "chat") {
+                  const roomId = parsedata.roomId;
+                  const message = parsedata.message;
+
+                  users.forEach(user => {
+                        if(user.rooms.includes(roomId)) {
+                              user.ws.send(JSON.stringify({
+                                    type:"chat",
+                                    message:message,
+                                    roomId
+                              }))
+                        }
+                  })
+            }
+      });
 });
 
